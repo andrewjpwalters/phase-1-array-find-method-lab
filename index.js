b926378c9e8d95b9b432c2842ccf85b6859fbbd8
Andrew Walters <andrewjpwalters@gmail.com>
2022-03-28 22:17:53 -0500

    commit tree 0ca5f2be5a66e54f90f8b7ed2b3357af72c6c7e4

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // code your solution here
+// function superbowlWin(array) {
+//     if (array.find(item => item.result === 'W')) {
+//         return item.year
+//     } else {
+//         return undefined
+//     }
+// }
+
+// function superbowlWin(array) {
+//     const isWin = array.find(item => item.result === 'W')
+//     if (isWin === true) {
+//         return item.year
+//     } else {
+//         return undefined
+//     }
+// }
+
+// function superbowlWin(array) {
+//     array.find(item => item.result === 'W')
+//     return item.year
+// }
+
+// function superbowlWin(array) {
+//     let win = array.find(item => item.result === 'W')
+//     if (win.result === 'W') {
+//         return win.year
+//     }
+//     else {
+//         return array.find(item => item.result === 'W')
+//     }
+// }
+
+function superbowlWin(array) {
+    if (array.find(item => item.result === 'W')) {
+        return array.find(item => item.result === 'W').year
+    } else {
+        return undefined
+    }
+}
